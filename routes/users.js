c33ebe8c2fdc191d977dea3590e0bed075f7479c
Shady Gabal <shady@nyu.edu>
2016-11-19 21:36:26 -0500

    user, forecast, weatherforzipcode skeletons

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var WeatherAtZipcode = require('app/db/WeatherAtZipcode');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.put('/', function(req, res){
+});
+
+
 
 module.exports = router;
