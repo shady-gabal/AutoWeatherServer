025666f8b7bcfa8dfbc107d21bb89de00e11b719
Shady Gabal <shady@nyu.edu>
2016-12-26 13:20:31 -0500

    force update

--- app.js
+++ app.js
@@ -134,8 +134,8 @@ passport.deserializeUser(function(user, done) {
 });
 
 
-//new CronJob('0 1,27 * * * *', function() {
-new CronJob('0 15 * * * *', function() {
+new CronJob('0 1,27 * * * *', function() {
+//new CronJob('0 15 * * * *', function() {
   updateWeatherAtZipcode();
 }, null, true, 'America/New_York');
 
